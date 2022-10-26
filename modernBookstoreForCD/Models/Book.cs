3c0b6a63f18a9ba87224c1e5a2664fbe3c0a43bc
Anders Børjesson <anbo@zealand.dk>
2022-10-26 19:16:08 +0200

    Add project files.

---
+++ modernBookstoreForCD/Models/Book.cs
@@ -0,0 +1,42 @@
+﻿namespace modernBookstoreForCD.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Price { get; set; }
+
+        override
+        public string ToString()
+        {
+            return $"Id: {Id}, Title: {Title}, Price: {Price}";
+        }
+
+        public void ValidateTitle()
+        {
+            if (Title == null)
+            {
+                throw new ArgumentNullException("Title is null");
+            }
+            if (Title.Length < 2)
+            {
+                throw new ArgumentException("Title must be at least 2 charaters: " + Title);
+            }
+        }
+
+        public void ValidatePrice()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Price must be greater than 0: " + Price);
+            }
+
+        }
+
+        public void Validate()
+        {
+            ValidateTitle();
+            ValidatePrice();
+        }
+    }
+}
---
+++ modernBookstoreForCDTests/Models/BookTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using modernBookstoreForCD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modernBookstoreForCD.Models.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
